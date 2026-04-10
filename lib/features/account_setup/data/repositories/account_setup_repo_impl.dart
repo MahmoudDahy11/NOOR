57360f8a17e866037beb82f4a003bf6e3728c7b7
Mahmoud Dahy <dahym2028@gmail.com>
2026-04-10 17:32:59 +0200

    feat: Implement AccountSetupRepo with saveProfile method for user profile management

---
+++ lib/features/account_setup/data/repositories/account_setup_repo_impl.dart
@@ -0,0 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dartz/dartz.dart';
+import 'package:tally_islamic/core/error/failure.dart';
+
+import '../../domain/entities/user_profile_entity.dart';
+import '../../domain/repositories/account_setup_repo.dart';
+import '../models/user_profile_model.dart';
+
+class AccountSetupRepoImpl implements AccountSetupRepo {
+  final FirebaseFirestore _firestore;
+
+  AccountSetupRepoImpl({FirebaseFirestore? firestore})
+      : _firestore = firestore ?? FirebaseFirestore.instance;
+
+  @override
+  Future<Either<CustomFailure, void>> saveProfile(
+    UserProfileEntity profile,
+  ) async {
+    try {
+      final model = UserProfileModel.fromEntity(profile);
+      await _firestore
+          .collection('users')
+          .doc(profile.uid)
+          .set(model.toFirestore());
+      return right(null);
+    } catch (e) {
+      return left(CustomFailure(errMessage: e.toString()));
+    }
+  }
+}
---
+++ lib/features/account_setup/domain/repositories/account_setup_repo.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:tally_islamic/core/error/failure.dart';
+
+import '../entities/user_profile_entity.dart';
+
+abstract class AccountSetupRepo {
+  Future<Either<CustomFailure, void>> saveProfile(UserProfileEntity profile);
+}
